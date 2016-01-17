d29a5f82a978f23b718eee67b6240bded3796b10
Moeko Takagi <barbiepinkmindoll@gmail.com>
2016-01-17 23:23:56 +0900

    Placement of bishop (DFS)

--- bishop.c
+++ bishop.c
@@ -2,134 +2,94 @@
 将棋の駒の角は，斜め方向に升目があいている限りどこへでも移動できる。
 特に，空いているその先に敵の駒があれば，その位置に進んでその駒を取ることができる。
 さらに，角は成ると馬となり，上下左右４つのマスにある敵の駒も取ることができるようになる。
-将棋の駒にとって取ることのできる位置にある（敵の）駒を当たりの駒という。
-
+将棋の駒にとって取ることのできる位置にある駒を当たりの駒という。
 正整数 n について n×n の升目をもつ盤を考え，馬を互いに当たりにならないように配置することを考え，
-最大何個置けるか、またその最大個数における配置は何通りあるかを出力するプログラムを作れ。
-
+最大何個置けるか、またその最大個数における配置は何通りあるかを出力するプログラムを作る。
 入力は，正整数 n だけを並べた１行からなる。
-出力は，配置できる最大個数とその最大個数における配置は何通りあるかを空白で区切り、末尾には改行を書き出せ。
-
-行iを0スタートと1スタートでそれぞれi += n+1していく。
+出力は，配置できる最大個数とその最大個数における配置は何通りあるかを空白で区切り、末尾には改行を書き出す。
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX 100 //jが置かれているかどうか
+#define MAX 100
 
-int depth_first_search_left(int i); //深さ優先探索
-int depth_first_search_right(int i); //深さ優先探索
-void set_bishop(int x);
-void remove_bishop(int x);
-bool prompted_bishop(); //馬になったとき当たりの駒があるかどうか
+int depth_first_search_left(int i); //中央より左側の深さ優先探索
+int depth_first_search_right(int i); //中央を含む右側の深さ優先探索
+void set_bishop(int x); //角を置く
+void remove_bishop(int x); //角を除く
+bool prompted_bishop(); //当たりの駒があるかどうか調べ馬に成る
 void print_state(); //現在の駒の状態を表示する
-bool search_slanting_bishop(int i); //斜め方向全てに馬がなかったらtrueを返す
+bool search_slanting_bishop(int i); //斜め方向全てに角がなかったらtrueを返す
 void count_bishop(); //馬の数を数える
 
 static int n;
-static bool hit[MAX] = {false};
-static int max_counter = 0;
+static int max_counter = 0; //馬を配置できる最大個数
+static int place_patterns = 0; //最大個数における配置数
 static bool node[MAX];
-static int place_patterns = 0;
 
 int main(){
 	scanf("%d", &n);
 	depth_first_search_left(1);
 	printf("%d %d\n", max_counter, place_patterns);
-
 }
 
 int depth_first_search_left(int i){
-	int x = i; //xはjを呼ぶ回数かつ引く数
-	i = n*(n-i);
-	int c = i;
-
+	int x = i; //x列
+	i = n*(n-i); //i行の1列目の場所
 	int j = 0;
 	while(j < x){
-		if(search_slanting_bishop(i) == true){ //j列に馬が置かれていなければ
-			set_bishop(i); //iに馬を置く
-	//		printf("馬を置いた場所%d\n", i);
-			if(i % (n+1) == 0){
+		if(search_slanting_bishop(i)){ //左隣の列に角が置かれていなければ
+			set_bishop(i); //i番目に角を置く
+			if(i % (n+1) == 0){ //中央まで探索済なら1番目の列を探索する
 				depth_first_search_right(1);
 			}else{
-				depth_first_search_left(x+1); //次の行で探索。左側にいる場合
+				depth_first_search_left(x+1); //中央より左側なら次の行を探索する
 			}
-			remove_bishop(i);
+			remove_bishop(i); //i番目の角を除く
 		}
-		i += n+1;
+		i += n+1; //次の列を探索
 		j++;
 	}
-
-//	print_state();	
-//	if(prompted_bishop() == true){
-//		print_state();
-//		printf("きた\n");
-//		count_bishop();
-//	}
-	if(c % (n+1) == 0){
-	//	printf("cは%d\n", c);
-		depth_first_search_right(1);
-	}else{
-		depth_first_search_left(x+1);
-	}
+	if(i % (n+1) == 0) depth_first_search_right(1);
+	else depth_first_search_left(x+1);
 	return 1;
 }
 
 int depth_first_search_right(int i){
+	//最大行数を超えたら全ての升に当たらないように置けたことになるので馬に成るかチェックし馬の数を数える
 	if(i >= n){
-		if(prompted_bishop() == true){
-	//		printf("こっち\n");
-//			print_state();
-			count_bishop();
-		}
-//		print_state();
+		if(prompted_bishop()) count_bishop();
 		return 1;
 	}
-//	printf("右\n");
-	int x = n-i; //xはjを呼ぶ回数
-	int c = i;
+	int x = n-i; //x列
+	int c = i; //c行
 	int j = 0;
-//	printf("%dがjを呼ぶ回数は%d\n", i, x);
 	while(j < x){
-		if(search_slanting_bishop(i) == true){ //j列に馬が置かれていなければ
-			set_bishop(i); //iに馬を置く
-	//		printf("馬を置いた場所%d\n", i);
+		if(search_slanting_bishop(i)){ //左隣の列に角が置かれていなければ
+			set_bishop(i); //i番目に角を置く
 			depth_first_search_right(c+1); //次の行で探索
-			remove_bishop(i); //iの馬を除く
-	//		printf("除いた馬%d\n", i);
+			remove_bishop(i); //i番目の角を除く
 		}
-		i += n+1;
+		i += n+1; //次の列を探索
 		j++;
 	}
-	if(c == n-1){
-		if(prompted_bishop() == true){
-	//		printf("ココ\n");
-	//		print_state();
-			count_bishop();
-		}
-//		print_state();
+	if(c == n-1){ //最後の行まで見て、馬になるものを探し馬を数える
+		if(prompted_bishop()) count_bishop();
 		return 1;
-	}else{
+	}else{ //最後の行でなければ次の行を探索
 		depth_first_search_right(c+1);
 	}
 	return 1;
 }
 
 bool search_slanting_bishop(int i){
-	if(i % n == 0){
-		return true;
-	}
-//	printf("iが%dのとき\n", i);
+	if(i % n == 0) return true; //一番左側なら同じ列に角はないとする
 
-	while(i < n*n && i % n != 0){
+	while(i < n*n && i % n != 0){ //i列目が一番左側じゃなくマス目を越えないとき馬に成るかチェック
 		i += n-1;
-	//	printf("iは%d\n", i);
-		if(node[i] == true){
-			return false;
-		}
-		
+		if(node[i] == true) return false;
 	}
 	return true;
 }
@@ -144,60 +104,42 @@ void remove_bishop(int x){
 
 bool prompted_bishop(){
 	int i;
-//	print_state();
 	for(i = 0; i < n*n; i++){
-		if(node[i] == true && i == 0){ //左上
-
-			if(node[1] == true || node[n] == true){
-		//		print_state();
-				return false;
-			}
-		}else if(node[i] == true && i == n-1){ //右上
-			if(node[i-1] == true || node[i+n] == true) return false; 
-		}else if(node[i] == true && i == n*(n-1)){ //左下
-			if(node[i-n] == true || node[i+1] == true) return false;
-		}else if(node[i] == true && i == n*n-1){ //右下
-			if(node[i-1] == true || node[i-n] == true) return false;
-		}else if(node[i] == true && i%n == 0){ //左
-			if(node[i+1] == true || node[i-n] == true || node[i+n] == true) return false;
-		}else if(node[i] == true && i < n){ //上
-			if(node[i-1] == true || node[i+1] == true || node[i+n] == true) return false; 
-		}else if(node[i] == true && i % n == n-1){ //右
-			if(node[i-1] == true || node[i-n] == true || node[i+n] == true) return false;
-		}else if(node[i] == true && i > n*(n-1)){ //下
-			if(node[i-n] == true || node[i+1] == true || node[i-1] == true) return false;
-		}else if(node[i] == true){ //それ以外
-			if(node[i-1] == true || node[i+1] == true || node[i-n] == true || node[i+n] == true){
-		//		print_state();
-				return false;
-			}
+		if(node[i] && i == 0){ //左上
+			if(node[1] || node[n]) return false;
+		}else if(node[i] && i == n-1){ //右上
+			if(node[i-1] || node[i+n]) return false; 
+		}else if(node[i] && i == n*(n-1)){ //左下
+			if(node[i-n] || node[i+1]) return false;
+		}else if(node[i] && i == n*n-1){ //右下
+			if(node[i-1] || node[i-n]) return false;
+		}else if(node[i] && i%n == 0){ //左
+			if(node[i+1] || node[i-n] || node[i+n]) return false;
+		}else if(node[i] && i < n){ //上
+			if(node[i-1] || node[i+1] || node[i+n]) return false; 
+		}else if(node[i] && i % n == n-1){ //右
+			if(node[i-1] || node[i-n] || node[i+n]) return false;
+		}else if(node[i] && i > n*(n-1)){ //下
+			if(node[i-n] || node[i+1] || node[i-1]) return false;
+		}else if(node[i]){ //それ以外
+			if(node[i-1] || node[i+1] || node[i-n] || node[i+n]) return false;
 		}
-	}
-//	print_state();
-	
-	return true; //途中で戻らなければ馬に昇格
+	}	
+	return true; //馬に昇格
 }
 
 void count_bishop(){
 	int i, x = 0;
 	for(i = 0; i < n*n; i++){
-	//	printf("%d ", node[i]);
-		if(node[i] == true){
-			x++;
-		}
+		if(node[i]) x++; //馬の数
 	}
-//	printf("x = %d\n", x);
-//	printf("\n");
 	if(x > max_counter){
-//		print_state();
 		max_counter = x;
 		place_patterns = 1;
 	}else if(x == max_counter){
-//		print_state();
 		place_patterns++;
 	}
 }
-
 
 void print_state(){
 	int i, j;
